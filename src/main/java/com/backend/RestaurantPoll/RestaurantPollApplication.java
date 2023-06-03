15c3a24bed9007cd14f87f9953b0dc467a9f30cd
parkk <parkkhomenko.a@gmail.com>
2023-06-03 23:59:58 +0300

    initial project setup

---
+++ src/main/java/com/backend/RestaurantPoll/RestaurantPollApplication.java
@@ -0,0 +1,13 @@
+package com.backend.RestaurantPoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantPollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantPollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/RestaurantPoll/RestaurantPollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.RestaurantPoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantPollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
